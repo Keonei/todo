eb157d87bb544f8b3e42eb7cf10e804d06e47550
kerry <keonei@uw.edu>
2017-06-19 02:11:01 -0700

    cleaned up code, lists name, radio code is written

--- script.js
+++ script.js
@@ -1,18 +1,51 @@
 function addName() {
+  console.log("Epic Adventure Time!");
   event.preventDefault();
 
-  var rad = document.getElementsByTagName("radio");
-    document.getElementsByTagName("radio").checked = true;
+  var rad = document.getElementsByName('class');
+  var taskClassObj = [];
+
+    for(var i = 0; i < rad.length; i++) {
+      rad[i].onclick = function() {
+        taskClassObj.push = ({"class":this.value});
+      };
+    };
+
+    for (var i = 0; i < taskClassObj.length; i++) {
+      if (i == 0)
+        document.getElementById("team-list").appendChild(li);
+      };
 
   var li = document.createElement("li");
-  var inputValue = document.getElementById("teamMember").value;
-  var t = document.createTextNode(inputValue);
+  var name = document.getElementById("teamMember").value;
+  var t = document.createTextNode(name);
 
   li.appendChild(t);
-  if (inputValue === '') {
+  if (name === '') {
     alert("You Cannot Journey Alone!");
   } else {
     document.getElementById("team-list").appendChild(li);
   }
   document.getElementById("teamMember").value = "";
 }
+
+
+// var difficulty = [
+//   "easy",
+//   "normal",
+//   "challenging",
+//   "hard",
+//   "stop",
+//   "nope"
+// ];
+//
+// var taskClass = [
+//   "paladin",
+//   "cleric",
+//   "fighter",
+//   "ranger",
+//   "rogue",
+//   "wizard"
+// ];
+//
+// var taskMember = [];
