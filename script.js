b7725676d6279474dd8fc9826bc083d9e29ef28e
kerry <keonei@uw.edu>
2017-06-21 00:46:42 -0700

    cleaned up code, simplified code

--- script.js
+++ script.js
@@ -1,51 +1,44 @@
-function addName() {
-  console.log("Epic Adventure Time!");
+console.log("Epic Adventure Time!");
+
+// create an object array for our selections
+var taskList = {
+  members: [
+
+]};
+
+// creates the unordered list for output
+function formData () {
+  var teamList = document.getElementById("team-list")
+  // document.getElementsByTagName("form").reset();
+
+  var ul = document.createElement("ul")
+    for(var i = 0; i < taskList.members.length; i++) {
+      console.log(taskList.members[i])
+
+      var li = document.createElement("li");
+        li.innerHTML = taskList.members[i]
+        // append the li to the ul elements
+        ul.appendChild(li)
+      }
+      // append the ul to the parent div
+      teamList.appendChild(ul)
+    }
+
+function addList () {
   event.preventDefault();
-
-  var rad = document.getElementsByName('class');
-  var taskClassObj = [];
-
-    for(var i = 0; i < rad.length; i++) {
-      rad[i].onclick = function() {
-        taskClassObj.push = ({"class":this.value});
-      };
-    };
-
-    for (var i = 0; i < taskClassObj.length; i++) {
-      if (i == 0)
-        document.getElementById("team-list").appendChild(li);
-      };
-
-  var li = document.createElement("li");
-  var name = document.getElementById("teamMember").value;
-  var t = document.createTextNode(name);
-
-  li.appendChild(t);
-  if (name === '') {
-    alert("You Cannot Journey Alone!");
-  } else {
-    document.getElementById("team-list").appendChild(li);
-  }
-  document.getElementById("teamMember").value = "";
+  // locate form section and find our values
+  // lets use querySelector since there is only one form
+  var form = document.querySelector("form");
+  // our task in this case is player selection for our team, input our values
+  var taskMember = form.teamMember.value + "\xa0has chosen to be the\xa0" + form.memberClass.value + "\xa0for your party and has requested difficulty\xa0" + document.getElementById("difficulty").value;
+  // push selection to our array
+  taskList.members.push(taskMember);
+  formData();
 }
 
+window.onload = function() {
+  formData();
 
-// var difficulty = [
-//   "easy",
-//   "normal",
-//   "challenging",
-//   "hard",
-//   "stop",
-//   "nope"
-// ];
-//
-// var taskClass = [
-//   "paladin",
-//   "cleric",
-//   "fighter",
-//   "ranger",
-//   "rogue",
-//   "wizard"
-// ];
-//
-// var taskMember = [];
+  var form = document.querySelector("form")
+  form.onsubmit = addList;
+}
